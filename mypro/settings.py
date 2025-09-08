a218193c19349bee205a2ef1ec0e254a8ee79f19
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-09-08 16:35:01 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -184,6 +184,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
