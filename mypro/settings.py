4afac42904eab71b72cf0803648ba8b6bb8f3e8a
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-09-08 17:19:22 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -149,15 +149,18 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-       "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 60,
+        "OPTIONS": {"sslmode": "require"},
     }
 }
+
 
 
 
