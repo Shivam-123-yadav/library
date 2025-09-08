50b033d2cd9153a2a619487ea60e432a0a916f32
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-09-08 17:14:34 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -123,16 +123,17 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),       # Database name from Render
-        'USER': os.getenv('DB_USER'),       # Username from Render
-        'PASSWORD': os.getenv('DB_PASSWORD'), # Password from Render
-        'HOST': os.getenv('DB_HOST'),       # Hostname from Render
-        'PORT': os.getenv('DB_PORT', '5432'), # Port from Render
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 60,
+        "OPTIONS": {"sslmode": "require"},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
