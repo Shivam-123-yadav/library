83485d738204c0959da1fd1fe2521e971e8a9481
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-11-21 15:30:22 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -130,11 +130,12 @@ LOGIN_REDIRECT_URL = '/book_list/'   # login ke baad kidhar bhejna hai
 LOGOUT_REDIRECT_URL = '/login/'      # logout ke baad kidhar bhejna hai
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='klickitshivam@gmail.com'
+EMAIL_HOST_PASSWORD='cvfl laws qivl pnmq'
+ORDER_NOTIFICATION_EMAIL = 'klickitshivam@gmail.com'
 # settings.py
 ORDER_NOTIFICATION_EMAIL = os.getenv("ORDER_NOTIFICATION_EMAIL")
 
