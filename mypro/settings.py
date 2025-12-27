0df6a89f348eb1c725a0b778a189253a53456a17
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2025-12-27 12:03:17 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -178,7 +178,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # EMAIL CONFIGURATION
 # =======================
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
